35b1d70ad76b44f074c4975e8e3b953ea8c9168f
AdministradorIT <git@tdrobotica.co>
2026-04-28 16:29:18 -0500

    feat: fix final 28 04 26

--- src/extensions/playiot/blocks.js
+++ src/extensions/playiot/blocks.js
@@ -1743,7 +1743,6 @@ class PlayIoTBlocks {
         const pin = args.PIN;
         const key = `din_${pin}`;
         const val = this.peripheral.sensorData[key];
-        console.log(`[digitalRead] pin=${pin} key=${key} val=${val} result=${val === 1 || val === true}`);
         return val === 1 || val === true;
     }
 
